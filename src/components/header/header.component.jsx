146d238f268941a49af2686ca37c8d0be6344f32
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-04-27 21:41:45 +0200

    Shop auf Redux umgebaut

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/original.svg';
@@ -7,7 +8,7 @@ import { ReactComponent as Logo } from '../../assets/original.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
-   /* <BrowserRouter>*/
+   
     <div className='header'>
        
         <Link className='logo-container' to="/">
@@ -29,7 +30,11 @@ const Header = ({ currentUser }) => (
         </div>
        
     </div>
-   /* </BrowserRouter>*/
+   
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps) (Header);
