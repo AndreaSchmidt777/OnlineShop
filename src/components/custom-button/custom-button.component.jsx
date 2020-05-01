69abfcef39c1c1f02e6f9485314addb9bcc0f35e
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-05-01 18:46:44 +0200

    Funktionalität atwas in den Warenkorb zu legen

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children,isGoogleSignIn, ...ohterProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`} {...ohterProps}>
+const CustomButton = ({ children,isGoogleSignIn, inverted, ...ohterProps}) => (
+    <button 
+    className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in': '' } custom-button`} {...ohterProps}>
         {children}
     </button>
 );
