3a1868da4b33056beb957e2c02752b2f13f1dec2
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-05-03 18:13:49 +0200

    Erstellt die CheckoutPage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
-
 import HomePage from './pages/homepage/homepage.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { createUserProfileDocument, auth } from './firebase/firebase.utils';
@@ -52,6 +53,7 @@ class App extends React.Component {
       <Switch> 
        <Route exact path='/' component={HomePage} />
        <Route path='/shop' component={ShopPage} />
+       <Route exact path='/checkout' component={CheckoutPage} />
        <Route exact path='/signin' 
        render={() => 
        this.props.currentUser ? (<Redirect to='/' />)
