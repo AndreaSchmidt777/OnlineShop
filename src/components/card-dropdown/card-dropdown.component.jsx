01b996e30c8a0e29b9bb555b840fa40cf3ff0b79
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-05-03 15:02:12 +0200

    alle Components auf Reselect umgebaut

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
+
+
 
 import './card-dropdown.styles.scss';
 
@@ -19,8 +22,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 );
 
-const mapStateToProps = (state) => ({
-    cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector ({
+    cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps) (CartDropdown);
